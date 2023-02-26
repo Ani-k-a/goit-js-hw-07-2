adc35f784abd5453f449acd061ed514de302df2c
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-02-26 21:38:15 +0100

    add 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryImg = document.querySelector('.gallery');
+
+const imagesBlock = galleryItems.map(({ preview, original, description }) => {
+    const image = `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+    return image;
+}).join("");
+
+galleryImg.insertAdjacentHTML("beforeend", imagesBlock);
+
+let gallery;
+galleryImg.addEventListener('click', handleClick);
+
+function handleClick (event){
+  event.preventDefault();
+  new SimpleLightbox('.gallery a', {captions: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250});
+} 
